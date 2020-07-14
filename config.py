891f6d27d495e43246e2dd61bd84928bfeb0914c
Matthew Alighchi <31544591+Malighchi@users.noreply.github.com>
2020-07-14 10:30:34 -0700

    Add files via upload

--- config.py
+++ config.py
@@ -4,9 +4,10 @@ from datetime import datetime
 # This file contains the configuration parameters which will be used throughout your experiments
 use_cuda = cuda.is_available()
 
-n_epochs = 50
+n_epochs = 100
 batch_size = 12
-n_num_frames = 16
+n_frames = 32
+skew_weight = False
 
 learning_rate = 1e-3
 weight_decay = 1e-7
@@ -14,5 +15,6 @@ weight_decay = 1e-7
 model_id = datetime.now().strftime('%Y-%m-%d %H,%M,%S')
 save_dir = './SavedModels/Run_%s/' % model_id
 
-model_path = './SavedModels/modeel_50_0.5433.pth'
+model_path = './SavedModels/Run_2020-07-10 14,31,48/model_50_2.6897.pth'
 
+bce_w_logits = True
